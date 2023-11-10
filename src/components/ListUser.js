fa316c50581e03ccc895e39e5f635e2f9077f1ab
unknown <vivienkorosiszabo@gmail.com>
2023-11-10 12:33:03 +0100

    creataed GET from DB

--- src/components/ListUser.js
+++ src/components/ListUser.js
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 function ListUser() {
-  return <div>ListUser</div>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  function getUsers() {
+    axios
+      .get(process.env.REACT_APP_API_POST_URL)
+      .then((response) => {
+        console.log(response.data);
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Mobile</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+
+        <tbody>
+        {Array.isArray(users) && users.length > 0 ? (
+            users.map((user, key) => (
+              <tr key={key}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.mobile}</td>
+                <td>
+                  <Link to={`user/${user.id}/edit`} style={{marginRight:"10px"}}>Edit</Link>
+                  <button>Delete</button>
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="5">Nincs elérhető adat.</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default ListUser;
